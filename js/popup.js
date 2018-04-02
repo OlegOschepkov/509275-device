b4fb476220f85143202bdf8af1f21e16c682b9be
OlegOschepkov <36763883+OlegOschepkov@users.noreply.github.com>
2018-04-02 14:34:24 +0400

    2 попытка

--- js/popup.js
+++ js/popup.js
@@ -1,28 +1,51 @@
-var link = Document.querySelector(".map-popup");
-var popup = Document.querySelector(".modal-map");
-var close = popup.querySelector("modal-close");
-var form = popup.querySelector("form");
-var formMail = popup.querySelector("[id=feedback-email]");
-var fromText = popup.querySelector("[id=feedback-text]");
-var formName = popup.querySelector("[id=feedback-name]");
+var link = document.querySelector(".map-popup");
+var popup = document.querySelector(".modal-map");
+var close = popup.querySelector(".modal-close");
+
+var feedback = document.querySelector(".feedback-popup");
+var writeus = document.querySelector(".modal-write-us");
+var form = writeus.querySelector(".modal-feedback");
+var name = writeus.querySelector("[name=feedback-name]");
+var mail = writeus.querySelector("[name=feedback-email]");
+var text = writeus.querySelector("[name=feedback-text]");
+var closefeedback = writeus.querySelector(".modal-close");
 
 link.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.add("modal-show");
-  formName.focus();
 });
-
 close.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.remove("modal-show");
 });
 
+feedback.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  writeus.classList.add("modal-show");
+});
+closefeedback.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  writeus.classList.remove("modal-show");
+  writeus.classList.remove("modal-shake");
+});
 form.addEventListener("submit", function (evt) {
-  if (!formName.value || !formMail.value || !formText.value) {
+  if (!name) {
     evt.preventDefault();
-    console.log("Пожалуйста заполните все поля");
-  } else {
-    localStorage.setItem("formName", formName);
-    localStorage.setItem("formMail", formMail);
+    writeus.classList.remove("modal-shake");
+    writeus.offsetWidth = writeus.offsetWidth;
+    writeus.classList.add("modal-shake");
+  };
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (writeus.classList.contains("modal-show")) {
+      writeus.classList.remove("modal-show");
+      writeus.classList.remove("modal-show");
+    }
+    if (popup.classList.contains("modal-show")) {
+      popup.classList.remove("modal-show")
+    }
   }
 });
